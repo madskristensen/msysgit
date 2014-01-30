a8e4f5afecfc16da0acecd3bb357fa20b125fe1c
Drew Miller <nemo@half-ogre.com>
2014-01-29 17:05:29 -0800

    remove git-tfs/*.xml from release script

--- share/WinGit/copy-files.sh
+++ share/WinGit/copy-files.sh
@@ -108,6 +108,6 @@ sed 's@/git/contrib/completion@/etc@g' \
 	< $MSYSGITROOT/etc/profile > etc/profile &&
 cp $MSYSGITROOT/share/resources/git.ico etc/ &&
 cp $MSYSGITROOT/share/resources/git.ico share/git-gui/lib/git-gui.ico &&
-cp $MSYSGITROOT/share/git-tfs/{*.dll,*.exe,*.config,*.xml} bin &&
+cp $MSYSGITROOT/share/git-tfs/{*.dll,*.exe,*.config} bin &&
 find bin libexec -iname \*.exe -o -iname \*.dll | sort > etc/fileList-bindimage.txt ||
 exit 1
